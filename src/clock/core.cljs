687d395a16bbb443537fe5437206c1f488a9058d
Mochamad Lucky Pradana <13579812+ampersanda@users.noreply.github.com>
2018-03-14 10:33:29 +0700

    Update core.cljs

--- src/clock/core.cljs
+++ src/clock/core.cljs
@@ -8,57 +8,66 @@
   )
 
 (defn draw-state [state]
-  ; Clear the sketch by filling it with light-grey color.
-  (q/background 150)
+  (q/background 51)
+  
+  (let
+    [max-scale-h (- 1 (/ 1 12))     ;; get percentage of current hour, range value from 0-1
+     max-scale-m (- 1 (/ 1 60))     ;; get percentage of current minute, range value from 0-1
+     max-scale-s (- 1 (/ 1 60))     ;; get percentage of current seconds, range value from 0-1
+     ;; above values are half of the actual values, so we need to multiply it with 2π
+     
+     ;; mappings
+     h (q/map-range 
+         (if (> (q/hour) 12) (- (q/hour) 12) (q/hour))  ;; if hour more than 12, subtract with 12
+         0 11 
+         0 max-scale-h)
+     m (q/map-range 
+         (q/minute)
+         0 59 
+         0 max-scale-m)
+     s (q/map-range 
+         (q/seconds) 
+         0 59 
+         0 max-scale-s)
+     
+     ;; multiply values above with 2π
+     h-a (* q/TWO-PI h)
+     m-a (* q/TWO-PI m)
+     s-a (* q/TWO-PI s)]
+    
+    ;; setting the stroke thickness
+    (q/stroke-weight 8)
+    ;; setting the arc with not fill inside
+    (q/no-fill)
+    
+    ;; setting the stroke color
+    (let [halfw (/ (q/width) 2)
+          halfh (/ (q/height) 2)]
+      (q/with-translation [halfw halfh]
+        
+        (q/with-rotation [(* -1 q/HALF_PI)]
+          ;; drawing the arcs
+          ; drawing hours
+          (q/stroke 255 100 150)
+          (q/with-rotation [h-a]
+            (q/line 0 0 60 0))
+          (q/arc 0 0 400 400 0 h-a)
 
-  (let [max-scale-h (- 1 (/ 1 12))
-        max-scale-m (- 1 (/ 1 60))
-        max-scale-s (- 1 (/ 1 60))]
-    (let
-      [
-        h (q/map-range
-           (if (> (q/hour) 12) (- (q/hour) 12) (q/hour)) 0 11 0 max-scale-h)
-        m (q/map-range (q/minute) 0 59 0 max-scale-m)
-        s (q/map-range (q/seconds) 0 59 0 max-scale-s)
+          ; drawing minutes
+          (q/stroke 150 100 255)
+          (q/with-rotation [m-a]
+            (q/line 0 0 100 0))
+          (q/arc 0 0 360 360 0 m-a)
 
-        half-width (/ (q/width) 2)
-        half-height (/ (q/height) 2)
-        ]
-
-      ;; let body
-      (q/translate half-width half-height)
-      (q/rotate (* -1 q/HALF-PI))
-
-      (q/stroke-weight 8)
-      (q/no-fill)
-
-      (let [angle (* q/TWO-PI h)]
-        (q/stroke 255 100 150)
-        (q/arc 0 0 300 300 0 angle)
-
-        (q/push-style)
-        (q/rotate angle)
-        (q/line 0 0 60 0)
-        (q/pop-style))
-
-      (let [angle (* q/TWO-PI m)]
-        (q/stroke 150 100 255)
-        (q/arc 0 0 280 280 0 angle)
-
-        (q/push-style)
-        (q/rotate angle)
-        (q/line 0 0 70 0)
-        (q/pop-style))
-
-      (let [angle (* q/TWO-PI s)]
-        (q/stroke 150 255 100)
-        (q/arc 0 0 260 260 0 angle)
-
-        (q/push-style)
-        (q/rotate angle)
-        (q/line 0 0 128 0)
-        (q/pop-style)
-        ))))
+          ; drawing hours
+          (q/stroke 150 255 100)
+          (q/with-rotation [s-a]
+            (q/line 0 0 160 0))
+          (q/arc 0 0 320 320 0 s-a)
+          )
+        )
+      )
+    ))
 
 ; this function is called in index.html
 (defn ^:export run-sketch []
